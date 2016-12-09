b3d872c667e0308d7f443a535f6afa95ed9977aa
bolboa <ebolboa@gmail.com>
2016-12-09 00:35:47 -0500

    transfered video to canvas html5

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -7,51 +7,57 @@ class Layout extends React.Component {
 		super();
 		this.localStream = '';
 		this.local2dContext = '';
+		this.localCanvas = '';
+		this.localCanvasLoop = '';
 	}
 
 	componentDidMount() {
+
+		var self = this;
+
+		this.localCanvas = this.refs.localCanvas.getContext('2d');
+
+		var streamVideoToCanvas = (vid,ctx)=>{
+			ctx.drawImage(vid,0,0,vid.width,vid.height);
+		}
 
 		navigator.getUserMedia_ = (   navigator.getUserMedia
                            || navigator.webkitGetUserMedia 
                            || navigator.mozGetUserMedia 
                            || navigator.msGetUserMedia);
 
-<<<<<<< HEAD
 		navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
       		function(stream) {
       			this.localStream = stream;
 				this.refs.localStream.src = window.URL.createObjectURL(this.localStream);
-				this.refs.localStream.play();
+				//this.refs.localStream.play();
 			}.bind(this),
 			function(err) {
 				console.log("nope");
 			});
-		}
 
-=======
-		navigator.getUserMedia({video:true, audio:true}, stream => {
-			this.localStream = stream;
-			this.refs.localStream.src = window.URL.createObjectURL(this.localStream);
-		},function(err){
-			console.log("Error", err);
-		});
->>>>>>> 861f01477f7c9ee4be1265485143f039b9dfd0df
-
-	openTab() {
-		chrome.tabs.create({'url': chrome.extension.getURL('./src/index.html')});
+		this.refs.localStream.addEventListener('play', function(){
+			var self_func = this;
+			(function loop() {
+				if(!self_func.paused && !self_func.ended) {
+					self.localCanvas.drawImage(self_func,0,0);
+					setTimeout(loop, 1000 / 30);
+				}
+			})();
+		},0);
 	}
+
+	/*openTab() {
+		chrome.tabs.create({'url': chrome.extension.getURL('./src/index.html')});
+	}*/
 	render() {
 		
 		return (
 			<div>	
-<<<<<<< HEAD
 				<video ref='localStream' className='localCanvas' width="320" height="240" autoPlay></video>
-=======
-				<video ref='localStream' className='localCanvas' width="320" height="240"></video>
->>>>>>> 861f01477f7c9ee4be1265485143f039b9dfd0df
+
 				<button id="snap">Snap Photo</button>
-				<canvas id="canvas" width="640" height="480"></canvas>
-				<button onClick={this.openTab}>open</button>
+				<canvas ref="localCanvas" id="canvas" width="640" height="480"></canvas>
 			</div>
 		)
 	}
